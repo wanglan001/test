4fabbfb1f66bed1f948203cd17eaf9d26e7650af
wanglan001 <lan.w@hotmail.com>
2017-07-12 10:25:37 +0800

    # WARNING: head commit changed in the meantime
    test11

---
+++ src/com/wl/test/HelloWord.java
@@ -0,0 +1,19 @@
+package com.wl.test;
+
+public class HelloWord {
+
+	String value;
+	public HelloWord(String v) {
+		this.value=v;
+	}
+	
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+ 
+}
